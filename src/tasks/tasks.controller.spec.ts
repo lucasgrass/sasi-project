0b3fabdbb3022272f6056df2f78c8547e1194418
Lucas Grass <grassberaldo@gmail.com>
2025-06-20 14:02:07 -0300

    Unit tests and READ.md

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,20 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
-      providers: [TasksService],
+      providers: [
+        {
+          provide: TasksService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call service.create', () => {
+      const dto: CreateTaskDto = {
+        title: 'Title', description: 'Description' 
+      };
+      controller.create(dto);
+      expect(service.create).toHaveBeenCalledWith(dto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call service.findAll', () => {
+      controller.findAll();
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('update', () => {
+    it('should call service.update', () => {
+      const id = 'id';
+      const dto: UpdateTaskDto = { completed: true };
+      controller.update(id, dto);
+      expect(service.update).toHaveBeenCalledWith(id, dto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should call service.remove', () => {
+      const id = 'id';
+      controller.remove(id);
+      expect(service.remove).toHaveBeenCalledWith(id);
+    });
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('TasksService', () => {
   let service: TasksService;
@@ -14,5 +17,68 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a task', () => {
+      const dto: CreateTaskDto = {
+        title: 'Title',
+        description: 'Description'
+      };
+      const task = service.create(dto);
+      expect(task.title).toBe(dto.title);
+      expect(task.description).toBe(dto.description);
+      expect(task.completed).toBe(false);
+      expect(task.status).toBe('pending');
+      expect(task.id).toBeDefined();
+      expect(task.createdAt).toBeInstanceOf(Date);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all tasks', () => {
+      service.create({ title: 'Title 1', description: 'Description 1' });
+      service.create({ title: 'Title 2', description: 'Description 2' });
+      service.create({ title: 'Title 3', description: 'Description 3' });
+      const tasks = service.findAll();
+      expect(tasks.length).toBe(3);
+    });
+  });
+
+  describe('update', () => {
+    it('should update the task to done', () => {
+      const task = service.create({ title: 'Title', description: 'Description' });
+      const updateDto: UpdateTaskDto = { completed: true };
+      const updated = service.update(task.id, updateDto);
+      expect(updated.completed).toBe(true);
+      expect(updated.status).toBe('done');
+    });
+  });
+
+  describe('update', () => {
+    it('should update the task to peding', () => {
+      const task = service.create({ title: 'Title', description: 'Description' });
+      const updateDto: UpdateTaskDto = { completed: false };
+      const updated = service.update(task.id, updateDto);
+      expect(updated.completed).toBe(false);
+      expect(updated.status).toBe('pending');
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a task', () => {
+      const task = service.create({ title: 'Title', description: 'Description' });
+      service.remove(task.id);
+      expect(service.findAll().length).toBe(0);
+    });
+  });
+
+  it('should throw NotFoundException if updating task do not exist', () => {
+    const updateDto: UpdateTaskDto = { completed: true };
+    expect(() => service.update('id-not-exist', updateDto)).toThrow(NotFoundException);
+  });
+
+  it('should throw NotFoundException if removing task do not exist', () => {
+    expect(() => service.remove('id-not-exist')).toThrow(NotFoundException);
   });
 });
